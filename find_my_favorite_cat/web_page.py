da8d12974b8544ccb37965403adb5623170b387f
Hong Chen <me@hongchen.cz>
2018-09-29 23:41:42 -0600

    daily commit (too lazy to type)

--- find_my_favorite_cat/web_page.py
+++ find_my_favorite_cat/web_page.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, session, url_for
 from flask_dropzone import Dropzone
 from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 import os
@@ -13,15 +13,37 @@ app.config["DROPZONE_ALLOWED_FILE_TYPE"]   = "image/*"
 app.config["DROPZONE_REDIRECT_VIEW"]       = "results"
 
 # upload
-app.config["UPLOAD_PHOTOS_DEST"] = os.path.join(os.getcwd(), "uploads")
-photo = UploadSet("photo", IMAGES)
-configure_uploads(app, photo)
+app.config["UPLOADED_PHOTOS_DEST"] = os.path.join(os.getcwd(), "uploads")
+photos = UploadSet("photos", IMAGES)
+configure_uploads(app, photos)
 patch_request_class(app)
 
-@app.route("/")
+app.config["SECRET_KEY"]= "hehehehahahahahh"
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+
+    if "file_urls" not in session:
+        session["file_urls"] = []
+
+    file_urls = session["file_urls"]
+
+    if request.method == 'POST':
+        file_obj = request.files
+        for f in file_obj:
+            file = request.files.get(f)
+            filename = photos.save(file, name=file.filename)
+            file_urls.append(photos.url(filename))
+            print(file.filename)
+
+        session["file_urls"] = file_urls
+        return "Uploading..."
     return render_template("index.html")
 
 @app.route("/results")
 def results():
-    return render_template("results.html")
+    if "file_urls" not in session or session["file_urls"]==[]:
+        return redirect(url_for("index"))
+    file_urls = session["file_urls"]
+    session.pop("file_urls", None)
+    return render_template("results.html", file_urls=file_urls)
