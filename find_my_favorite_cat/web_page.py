9c0d32e8f8ac1075969d609efdf7743c90850787
Hong Chen <me@hongchen.cz>
2018-09-30 21:22:30 -0600

    daily commit (too lazy to type)

--- find_my_favorite_cat/web_page.py
+++ find_my_favorite_cat/web_page.py
@@ -1,16 +1,21 @@
 from flask import Flask, redirect, render_template, request, session, url_for
+from flask_wtf.csrf import CSRFProtect, CSRFError
 from flask_dropzone import Dropzone
 from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 import os
 
 app = Flask(__name__)
 
+
 # dropzone for images
 dropzone = Dropzone(app)
-app.config["DROPZONE_UPLOAD_MULTIPLE"]     = True
-app.config["DROPZONE_ALLOWED_FILE_CUSTOM"] = True
-app.config["DROPZONE_ALLOWED_FILE_TYPE"]   = "image/*"
+app.config["DROPZONE_ALLOWED_FILE_TYPE"]   = "image"
+app.config["DROPZONE_MAX_FILES"]           = 1
+app.config["DROPZONE_MAX_FILE_SIZE"]       = 25
 app.config["DROPZONE_REDIRECT_VIEW"]       = "results"
+app.config["DROPZONE_ENABLE_CSRF"]         = True
+app.config["DROPZONE_DEFAULT_MESSAGE"]     = "<h3><span style=\"color:#adb4ba\">Please drop a image of your favorite cat here:</span></h3>"
+app.config["DROP_ZONE_TIMEOUT"]            = 300000
 
 # upload
 app.config["UPLOADED_PHOTOS_DEST"] = os.path.join(os.getcwd(), "uploads")
@@ -18,8 +23,9 @@ photos = UploadSet("photos", IMAGES)
 configure_uploads(app, photos)
 patch_request_class(app)
 
-app.config["SECRET_KEY"]= "hehehehahahahahh"
+app.config["SECRET_KEY"]= "my secret key"
 
+csrf = CSRFProtect(app)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -53,3 +59,14 @@ def results():
     file_urls = session["file_urls"]
     session.pop("file_urls", None)
     return render_template("results.html", file_urls=file_urls)
+
+
+
+@app.errorhandler(CSRFError)
+def csrf_error(e):
+    return e.description, 400
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
